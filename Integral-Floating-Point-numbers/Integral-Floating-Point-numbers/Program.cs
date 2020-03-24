afa6c7a0a65a99c80a4b8bc6c0a511aa4034d802
Keith Heffington <lightingcount_@hotmail.com>
2020-03-24 12:44:49 -0500

    Integer Introduction

---
+++ Integral-Floating-Point-numbers/Integral-Floating-Point-numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Integral_Floating_Point_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 18; //int type represents an integer, a positive or negatice whole number
+            int b = 6;
+
+            int c = a + b; // + symbol for addition
+            Console.WriteLine($"{a} + {b} = {c} Addition");
+
+            c = a - b; // - symbol for subtraction
+            Console.WriteLine($"{a} - {b} = {c} Subtraction");
+
+            c = a * b; // * symbol for multiplication
+            Console.WriteLine($"{a} * {b} = {c} Multiplication");
+
+            c = a / b; // / symbol for division
+            Console.WriteLine($"{a} / {b} = {c} Division");
+        }
+    }
+}
