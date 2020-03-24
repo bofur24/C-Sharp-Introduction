b78632e1b16635433b9388a046414569e15961e9
Keith Heffington <lightingcount_@hotmail.com>
2020-03-24 10:28:51 -0500

    First Build

---
+++ Declare-Use-Variables/Declare-Use-Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Declare_Use_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+        }
+    }
+}
