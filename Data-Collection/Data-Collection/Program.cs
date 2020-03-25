6088c0896b8e3a29dab5e369ad306b6d1e46928c
Keith Heffington <lightingcount_@hotmail.com>
2020-03-25 15:38:40 -0500

    Create lists

---
+++ Data-Collection/Data-Collection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Lists
+
+            Console.WriteLine("Create Lists");
+            var names = new List<string> { "Keith", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
