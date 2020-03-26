7d3a8dae11531b2638d5065129708805c82e7d49
Keith Heffington <lightingcount_@hotmail.com>
2020-03-26 10:43:34 -0500

    Modify List Contents

--- Data-Collection/Data-Collection/Program.cs
+++ Data-Collection/Data-Collection/Program.cs
@@ -15,6 +15,24 @@ namespace Data_Collection
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+            Console.WriteLine("");
+
+            // Modify List Contents
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"My name is {names[0]}.");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+            Console.WriteLine("");
+
+            Console.WriteLine($"The list has {names.Count} people in it.");
         }
     }
 }
